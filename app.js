9e0445c1231bd30265849fea1c7709edf8651882
Krishna Bansal <kana.sonkh@gmail.com>
2022-12-20 15:21:59 +0530

    fir

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express();
 const port=process.env.PORT || 5000;
 
 app.get("/",(req,res)=>{
-    res.json({"name":"Krishna Agrawal"})
+    res.setHeader('Content-Type', 'application/json').json({"name":"Krishna Agrawal"})
 })
 app.listen(port,()=>{
     console.log(`the app is listen at ${port}`)
